5bc22200907b9575f1c3535b39ca6fb747a36fbc
jyothsna <telu.jyothsna@gmail.com>
2015-03-17 12:31:19 +0530

    scroll initialized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,32 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var Edulastic = (function(window, $){
+	var initializeScrollBars = function(){
+		// var $customScrollbars = $('div[data-custom-scrollbar="true"]');
+		// $customScrollbars.mCustomScrollbar();
+
+
+		$('#rightbar .js-scroll-bar').mCustomScrollbar({
+				setHeight: "100%",
+				theme: "dark",
+				setTop: "-110px"
+			});
+	};
+
+	var listenForDomInsertionsToInitializeScrollbars = function(){
+
+	}
+
+	function Edulastic(){
+		initializeScrollBars();
+		listenForDomInsertionsToInitializeScrollbars();
+	}
+
+	return{
+		initializeScrollbars: initializeScrollBars,
+		init: Edulastic
+	};
+
+})(window, $) ;	
+
+Edulastic.init();
